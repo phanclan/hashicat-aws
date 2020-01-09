6fc5a66caca820ee35a332f2f42ed6b9bce0f427
Peter Phan <pphan@hashicorp.com>
2020-01-09 01:25:00 +0000

    Updated VPC Module

--- vpc.tf
+++ vpc.tf
@@ -10,4 +10,6 @@ module "vpc" {
   private_subnet_assign_ipv6_address_on_creation = "false"
   public_subnet_assign_ipv6_address_on_creation = "false"
   redshift_subnet_assign_ipv6_address_on_creation = "false"
+
+  cidr = "10.0.0.0/16"
 }
