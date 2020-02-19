225eb24e456894722d732085499623e99ba9816f
Peter Phan <peterphan@hotmail.com>
2020-02-18 19:44:25 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -45,6 +45,6 @@ variable "width" {
 }
 
 variable "placeholder" {
-  default     = "placekitten.com"
-  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+  default     = "placedog.net"
+  description = "Image-as-a-service URL. Some other fun ones to try are placekitten.com, placedog.net, fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com"
 }
