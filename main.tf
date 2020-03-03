7ad784d0d4ea295b9609630857ee42e0c2dcef8b
Peter Phan <peterphan@Peters-MacBook-Pro-4.local>
2020-03-02 17:55:52 -0800

    update variables

--- main.tf
+++ main.tf
@@ -116,13 +116,13 @@ resource aws_instance "hashicat" {
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
-  tags = {
-    Name = "${var.prefix}-hashicat-instance"
+  # tags = {
+  #   Name = "${var.prefix}-hashicat-instance"
   #   Billable = true
   #   Department = "devops"
   #   TTL = "72hrs"
   #   Owner = "pphan@hashicorp.com"
-  }
+  # }
 }
 
 # We're using a little trick here so we can run the provisioner without
