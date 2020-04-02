27d8970fffca94422fcff831c5e6b25093f13e76
Peter Phan <pphan@hashicorp.com>
2020-04-02 11:12:13 -0700

    update variables

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,15 @@
-# module "vpc" {
-#   source  = "app.terraform.io/pphan-sandbox/vpc/aws"
-#   version = "2.21.0"
+module "vpc" {
+  source  = "app.terraform.io/pphan-sandbox/vpc/aws"
+  version = "2.21.0"
 
-#   database_subnet_assign_ipv6_address_on_creation = "false"
-#   elasticache_subnet_assign_ipv6_address_on_creation = "false"
-#   enable_classiclink = "false"
-#   enable_classiclink_dns_support = "false"
-#   intra_subnet_assign_ipv6_address_on_creation = "false"
-#   private_subnet_assign_ipv6_address_on_creation = "false"
-#   public_subnet_assign_ipv6_address_on_creation = "false"
-#   redshift_subnet_assign_ipv6_address_on_creation = "false"
+  database_subnet_assign_ipv6_address_on_creation = "false"
+  elasticache_subnet_assign_ipv6_address_on_creation = "false"
+  enable_classiclink = "false"
+  enable_classiclink_dns_support = "false"
+  intra_subnet_assign_ipv6_address_on_creation = "false"
+  private_subnet_assign_ipv6_address_on_creation = "false"
+  public_subnet_assign_ipv6_address_on_creation = "false"
+  redshift_subnet_assign_ipv6_address_on_creation = "false"
 
-#   cidr = "10.0.0.0/16"
-# }
+  cidr = "10.0.0.0/16"
+}
