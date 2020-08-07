be66c35809a014092e15bb508bbd6639047e8b40
Peter Phan <pphan@hashicorp.com>
2020-08-07 09:29:05 -0700

    update ssh key name

--- main.tf
+++ main.tf
@@ -195,7 +195,7 @@ resource tls_private_key "hashicat" {
 }
 
 locals {
-  private_key_filename = "${var.prefix}-ssh-key.pem"
+  private_key_filename = "${var.prefix}-ssh-key-hashicat.pem"
 }
 
 resource aws_key_pair "hashicat" {
