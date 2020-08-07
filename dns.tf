d94292fcaf18e6543fa2d8d9b83f872b9a931f10
Peter Phan <pphan@hashicorp.com>
2020-08-07 09:57:33 -0700

    update dns

--- dns.tf
+++ dns.tf
@@ -8,7 +8,7 @@ resource "aws_route53_record" "hashicat" {
 
 variable "zone_id" {
   description = "Hosted zone ID"
-  default = "Z1SE34SEPQ6H86"
+  # default = "Z1SE34SEPQ6H86"
 }
 
 output "hashicat_dns" {
