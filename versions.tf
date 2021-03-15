1b86ceba7dbcd10af6a925fb8d7972770d44f0bb
Peter Phan <pphan@hashicorp.com>
2021-03-15 13:27:58 -0700

    added versions.tf for provider config

---
+++ versions.tf
@@ -0,0 +1,27 @@
+#---------------------------------------------------------------------------
+# VERSIONS
+#---------------------------------------------------------------------------
+terraform {
+  required_providers {
+    acme = {
+      source  = "vancluever/acme"
+      version = "~> 2.0.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.25.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "aws" {
+  region  = var.region
+}
